023c31d1f33de7e8e96468c53c39c505c1526865
thanawong <tomta555@hotmail.com>
2019-10-29 02:03:31 +0700

    fixed beq , add label undefine check, add out of bound offsetField check, add wrong opcode check, add J-Type O-Type .fill

--- ASMtoMachine.py
+++ ASMtoMachine.py
@@ -41,15 +41,47 @@ def decimal_to_binary(val, bits):    # convert decimal int to binary string
             result += "0"
     return result                   # return string
 
+def is_number(val):
+    """Check if input string is a number"""
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
+
+
+def is_number_in_range(val,check):
+    """Check if number is in range"""
+    if(check == "offset"):
+        if ((int(val) >= -32768) and (int(val) <= 32767)):
+            return True 
+        else:
+            print("Error: offset out of range")
+            exit(1)
+    elif(check == "reg"):
+        if((int(val) >= 0) and (int(val) <= 7)):
+            return True
+        else:
+            print("Error: register number out of range")
+            exit(1)
+
+
+def is_label_defined(key):
+    """Check if label is defined"""
+    if key in _label:
+        return True
+    else:
+        print("Error: label undefine")
+        exit(1)
 
 def twos_comp(val, bits):
-    """compute the 2's complement of int"""
+    """compute the 2's complement of int for negative decimal to binary bits"""
     val = val - (1 << bits)         # compute negative value
     return -val                     # return positive value
 
 
 def writeData(data):  # write data to output.txt
-    f = open("output.txt", "a")
+    f = open("machineCode.txt", "a")
     f.write(data + "\n")
     f.close()
 
@@ -65,30 +97,33 @@ def inFileParse():
     for line in inFile:             # Read line
         # Split field to Array lineSplit[idx]
         lineSplit = line.split("    ")
-        if(lineSplit[1] == 'lw' or lineSplit[1] == 'sw' or lineSplit[1] == 'beq' or lineSplit[1] == 'add' or lineSplit[1] == 'nand'):
+        # print(lineSplit)
+        if(lineSplit[1] == 'lw' or lineSplit[1] == 'sw' or lineSplit[1] == 'beq' or lineSplit[1] == 'add' or lineSplit[1] == 'nand' or lineSplit[1] == 'jalr' or lineSplit[1] == 'halt' or lineSplit[1] == 'noop' or lineSplit[1] == '.fill'):
             if(lineSplit[1] == 'beq'):
                 opcode = '100'
                 # Check if field is Number
-                if lineSplit[2].isnumeric():
-                    # Format Number in field0 to Binary length 3 bits ({0:03b})
-                    field0 = decimal_to_binary(int(lineSplit[2]), 3)
+                if is_number(lineSplit[2]):
+                    if is_number_in_range(lineSplit[2],"reg"):
+                        field0 = decimal_to_binary(int(lineSplit[2]), 3)
                 else:
-                    field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
-                if lineSplit[3].isnumeric():
-                    field1 = decimal_to_binary(int(lineSplit[3]), 3)
+                    if is_label_defined(lineSplit[2]):
+                        field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
+                if is_number(lineSplit[3]):
+                    if is_number_in_range(lineSplit[3],"reg"):
+                        field1 = decimal_to_binary(int(lineSplit[3]), 3)
                 else:
-                    field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
-
-                if lineSplit[4].isnumeric():
-                    field2 = decimal_to_binary(int(lineSplit[4]), 16)
+                    if is_label_defined(lineSplit[3]):
+                        field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
+                if is_number(lineSplit[4]):
+                    if is_number_in_range(lineSplit[4],"offset"):
+                        field2 = decimal_to_binary(int(lineSplit[4]), 16)
                 else:
-                    if(int(_label[lineSplit[4]]) >= pc):
-                        field2 = decimal_to_binary(
-                            int(_label[lineSplit[4]] - pc), 16)
-                    else:                                                # If branch backward use 2's complement
-                        field2 = int(
-                            twos_comp(int(pc - _label[lineSplit[4]] + 1), 16))
-                        field2 = decimal_to_binary(field2, 16)
+                    if is_label_defined(lineSplit[4]):
+                        if int(_label[lineSplit[4]]) >= pc:
+                            field2 = decimal_to_binary(int(_label[lineSplit[4]] - pc - 1), 16)
+                        else:                                                # If branch backward use 2's complement
+                            field2 = int(twos_comp(int(pc - _label[lineSplit[4]] + 1), 16))
+                            field2 = decimal_to_binary(field2, 16)
 
             elif(lineSplit[1] == 'lw' or lineSplit[1] == 'sw'):
                 if(lineSplit[1] == 'lw'):
@@ -96,63 +131,100 @@ def inFileParse():
                 elif(lineSplit[1] == 'sw'):
                     opcode = '011'
 
-                if lineSplit[2].isnumeric():
-                    field0 = decimal_to_binary(int(lineSplit[2]), 3)
+                if is_number(lineSplit[2]):
+                    if is_number_in_range(lineSplit[2],"reg"):
+                        field0 = decimal_to_binary(int(lineSplit[2]), 3)
                 else:
-                    field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
-                if lineSplit[3].isnumeric():
-                    field1 = decimal_to_binary(int(lineSplit[3]), 3)
+                    if is_label_defined(lineSplit[2]):
+                        field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
+                if is_number(lineSplit[3]):
+                    if is_number_in_range(lineSplit[3],"reg"):
+                        field1 = decimal_to_binary(int(lineSplit[3]), 3)
                 else:
-                    field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
-                if lineSplit[4].isnumeric():
-                    field2 = decimal_to_binary(int(lineSplit[4]), 16)
+                    if is_label_defined(lineSplit[3]):
+                        field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
+                if is_number(lineSplit[4]):
+                    if is_number_in_range(lineSplit[4],"offset"):
+                        field2 = decimal_to_binary(int(lineSplit[4]), 16)
                 else:
-                    field2 = decimal_to_binary(int(_label[lineSplit[4]]), 16)
+                    if is_label_defined(lineSplit[4]):
+                        field2 = decimal_to_binary(int(_label[lineSplit[4]]), 16)
 
-            elif(lineSplit[1] == 'add'):
-                opcode = '000'
-                if lineSplit[2].isnumeric():
-                    field0 = decimal_to_binary(int(lineSplit[2]), 3)
+            elif(lineSplit[1] == 'add' or lineSplit[1] == 'nand'):
+                if(lineSplit[1] == 'add'):
+                    opcode = '000'
+                elif(lineSplit[1] == 'nand'):
+                    opcode = '001'
+                    
+                if is_number(lineSplit[2]):
+                    if is_number_in_range(lineSplit[2],"reg"):
+                        field0 = decimal_to_binary(int(lineSplit[2]), 3)
                 else:
-                    field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
-                if lineSplit[3].isnumeric():
-                    field1 = decimal_to_binary(int(lineSplit[3]), 3)
+                    if is_label_defined(lineSplit[2]):
+                        field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
+                if is_number(lineSplit[3]):
+                    if is_number_in_range(lineSplit[3],"reg"):
+                        field1 = decimal_to_binary(int(lineSplit[3]), 3)
                 else:
-                    field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
-                if lineSplit[4].isnumeric():
-                    field2 = decimal_to_binary(int(lineSplit[4]), 3)
+                    if is_label_defined(lineSplit[3]):
+                        field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
+                if is_number(lineSplit[4]):
+                    if is_number_in_range(lineSplit[4],"reg"):
+                        field2 = decimal_to_binary(int(lineSplit[4]), 3)
                 else:
-                    field2 = decimal_to_binary(int(_label[lineSplit[4]]), 3)
+                    if is_label_defined(lineSplit[4]):
+                        field2 = decimal_to_binary(int(_label[lineSplit[4]]), 3)
+            
+            elif(lineSplit[1] == 'jalr'):
+                opcode = '101'
+                if is_number(lineSplit[2]):
+                    if is_number_in_range(lineSplit[2],"reg"):
+                        field0 = decimal_to_binary(int(lineSplit[2]), 3)
+                else:
+                    if is_label_defined(lineSplit[2]):
+                        field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
+                if is_number(lineSplit[3]):
+                    if is_number_in_range(lineSplit[3],"reg"):
+                        field1 = decimal_to_binary(int(lineSplit[3]), 3)
+                else:
+                    if is_label_defined(lineSplit[3]):
+                        field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
 
-            elif(lineSplit[1] == 'nand'):
-                opcode = '001'
-                if lineSplit[2].isnumeric():
-                    field0 = decimal_to_binary(int(lineSplit[2]), 3)
+            elif(lineSplit[1] == 'halt' or lineSplit[1] == 'noop'):
+                if(lineSplit[1] == 'halt'):
+                    opcode = '110'
+                elif(lineSplit[1] == 'noop'):
+                    opcode = '111'
+
+            elif(lineSplit[1] == '.fill'):
+                opcode = lineSplit[2].replace('\n', '')
+                if is_number(lineSplit[2]):
+                    pass
                 else:
-                    field0 = decimal_to_binary(int(_label[lineSplit[2]]), 3)
-                if lineSplit[3].isnumeric():
-                    field1 = decimal_to_binary(int(lineSplit[3]), 3)
-                else:
-                    field1 = decimal_to_binary(int(_label[lineSplit[3]]), 3)
-                if lineSplit[4].isnumeric():
-                    field2 = decimal_to_binary(int(lineSplit[4]), 3)
-                else:
-                    field2 = decimal_to_binary(int(_label[lineSplit[4]]), 3)
+                    if is_label_defined(opcode):
+                        opcode = _label[opcode]
+            else:
+                print("Error : Undifined instruction")
+                exit(1)
 
             if(lineSplit[1] == 'add' or lineSplit[1] == 'nand'):
-                machineCode = binary_to_decimal(
-                    bit31_25 + opcode + field0 + field1 + bit15_3 + field2)
+                machineCode = binary_to_decimal(bit31_25 + opcode + field0 + field1 + bit15_3 + field2)
                 print(machineCode)
-
             elif(lineSplit[1] == 'lw' or lineSplit[1] == 'sw' or lineSplit[1] == 'beq'):
-                machineCode = binary_to_decimal(
-                    bit31_25 + opcode + field0 + field1 + field2)
+                machineCode = binary_to_decimal(bit31_25 + opcode + field0 + field1 + field2)
                 print(machineCode)
-
+            elif(lineSplit[1] == 'jalr'):
+                machineCode = binary_to_decimal(bit31_25 + opcode + field0 + field1 + bit15_0)
+                print(machineCode)
+            elif(lineSplit[1] == 'halt' or lineSplit[1] == 'noop'):
+                machineCode = binary_to_decimal(bit31_25 + opcode + bit21_0)
+                print(machineCode)
+            elif(lineSplit[1] == '.fill'):
+                machineCode = int(opcode)
+                print(machineCode)
             writeData(str(machineCode))  # write machine code to output.txt
         pc += 1
     inFile.close()
-
 
 # receive machine argument from command line ex. python ASMtoMachine.py in.txt
 inFilePath = sys.argv[1]
